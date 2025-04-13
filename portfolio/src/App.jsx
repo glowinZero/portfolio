da0628f3d80f846dfa23f021a172dfa643686be3
glowinZero <me@glowinzero.pt>
2025-04-13 23:07:02 +0100

    New project folder + clean page

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='main-div'>
+      <h1 className='main-heading'>Hello! This is glowinZero talking...</h1>
+    </div>
+  )
+}
+
+export default App
